da689a347b5cbf4efecabd0998193cdd449465e6
Guodong Sun <82430006+raymon-sun@users.noreply.github.com>
2022-09-02 23:50:19 +0800

    feat: initialize graph setter

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,21 @@ struct Commit {
     author_email: String,
     author_name: String,
     author_date: String,
+    graph: Vec<GraphSlice>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct GraphSlice {
+    commit_position: u32,
+    commit_color: String,
+    lines: Vec<CommitGraphLine>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct CommitGraphLine {
+    top: u32,
+    bottom: u32,
+    color: String,
 }
 
 #[wasm_bindgen]
@@ -26,5 +41,28 @@ pub fn greet(name: &str) {
 #[wasm_bindgen]
 pub fn attach_graph(val: JsValue) -> JsValue {
     let commits: Vec<Commit> = serde_wasm_bindgen::from_value(val).unwrap();
+
+    commits.iter().map(|commit| attach_graph_slice(commit));
+
     serde_wasm_bindgen::to_value(&commits).unwrap()
+}
+
+fn attach_graph_slice(original_commit: &Commit) -> Commit {
+    let commit =  Commit {
+        hash: original_commit.hash.clone(),
+        ref_names: original_commit.ref_names.clone(),
+        message: original_commit.message.clone(),
+        parents: original_commit.parents.clone(),
+        commit_date: original_commit.commit_date.clone(),
+        author_email: original_commit.author_email.clone(),
+        author_name: original_commit.author_name.clone(),
+        author_date: original_commit.author_date.clone(),
+        graph : vec![GraphSlice {
+            commit_position: 0,
+            commit_color: "".to_string(),
+            lines: vec![],
+        }]
+    };
+
+    commit
 }
